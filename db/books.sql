1f834fde78c3d8d3eea8cac1179f3c46c90be8d0
jnelbaylen <jnelbaylen@gmail.com>
2026-03-24 11:56:40 +0800

    feat: add price attribute

--- db/books.sql
+++ db/books.sql
@@ -10,3 +10,6 @@ CREATE TABLE books (
 -- INSERT INTO books
 -- VALUES
 --     (1, 'Sample Book', 'This is a test description for the sample book.', 'sample.jpg');
+
+ALTER TABLE books
+ADD COLUMN price INT NOT NULL;
