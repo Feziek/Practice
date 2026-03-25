567e7b70daf77f3d0687b6af02e6d63eb4ec4d9f
jnelbaylen <jnelbaylen@gmail.com>
2026-03-25 16:10:49 +0800

    chore: set up Tailwind CSS and add path aliases for cleaner imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve('src/assets'),
+      '@components': path.resolve('src/components'),
+      '@routes': path.resolve('src/routes'),
+      '@pages': path.resolve('src/pages'),
+      '@styles': path.resolve('src/styles')
+    }
+  },
 })
