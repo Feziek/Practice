7abeb20968c76fdc0812301c51a4dd21d94729d7
jnelbaylen <jnelbaylen@gmail.com>
2026-03-29 00:05:30 +0800

    chore: clean up and change varchar

--- db/books.sql
+++ db/books.sql
@@ -5,18 +5,5 @@ CREATE TABLE books (
     title VARCHAR(50) NOT NULL,
     description VARCHAR(255) NOT NULL,
     price DECIMAL (10, 2) NOT NULL,
-    cover VARCHAR(50) NULL
+    cover VARCHAR(255) NULL
 );
-
--- INSERT INTO books
--- VALUES
---     (1, 'Sample Book', 'This is a test description for the sample book.', 'sample.jpg');
-
--- ALTER TABLE books
--- ADD COLUMN price INT NOT NULL;
-
--- DELETE FROM books 
--- WHERE id BETWEEN 2 AND 10;
-
--- ALTER TABLE books
--- MODIFY cover VARCHAR(255);
