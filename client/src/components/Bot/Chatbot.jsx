84a742eba08497514741fdad57dc0ef71f802e25
jnelbaylen <jnelbaylen@gmail.com>
2026-04-04 13:57:24 +0800

    chore: change bot message

--- client/src/components/Bot/Chatbot.jsx
+++ client/src/components/Bot/Chatbot.jsx
@@ -36,7 +36,7 @@ export function Chatbot() {
         ...prev,
         { 
           type: "bot", 
-          text: "Loading....." 
+          text: "Thinking....." 
         }
       ])
     }, 800)
